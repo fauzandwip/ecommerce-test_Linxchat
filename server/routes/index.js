ef88c87f2aed1cda134728af2c14f3796d1599e7
fauzandwip <fauzan27dp@gmail.com>
2024-01-18 20:10:41 +0700

    feat: add authentication middleware

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,5 +1,6 @@
 const ProductController = require('../controllers/ProductController');
 const UserController = require('../controllers/UserController');
+const authentication = require('../middlewares/authentication');
 
 const router = require('express').Router();
 
@@ -7,5 +8,8 @@ router.get('/', (req, res) => res.send('LINXCHAT'));
 router.post('/register', UserController.createUser);
 router.post('/login', UserController.login);
 router.get('/products', ProductController.getProducts);
+
+router.use(authentication);
+router.get('/invoice/:id', (req, res) => res.send('invoice'));
 
 module.exports = router;
